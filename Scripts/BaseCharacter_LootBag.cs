b4c6c1948be94a8f7c8eab837b6c5a8ffd8e2f9a
vaughanb <brenden.vaughan@gmail.com>
2021-11-01 19:50:59 -0600

    Re-worked loot randomization algorithm to match SURIYUN's implementation.

--- Scripts/BaseCharacter_LootBag.cs
+++ Scripts/BaseCharacter_LootBag.cs
@@ -19,55 +19,24 @@ namespace MultiplayerARPG
         public LootBagEntity lootBagEntityOverride;
         public ItemDrop[] randomLootBagItems;
         public ItemDropTable[] lootBagItemDropTables;
-        public int maxRandomLootItems = 5;
+        public int maxLootItems = 5;
         public float lootBagDestroyDelay = 30;
 
         [System.NonSerialized]
-        private List<ItemDrop> certainDropItems = new List<ItemDrop>();
+        private List<ItemDrop> certainLootItems = new List<ItemDrop>();
         [System.NonSerialized]
-        private List<ItemDrop> uncertainDropItems = new List<ItemDrop>();
+        private List<ItemDrop> uncertainLootItems = new List<ItemDrop>();
+
         [System.NonSerialized]
-        protected List<ItemDrop> cacheRandomLootBagItems;
-
-        /// <summary>
-        /// Returns a random collection of item drops.
-        /// </summary>
-        /// <returns>list of random item drops</returns>
-        public List<CharacterItem> GetRandomItems()
-        {
-            List<CharacterItem> LootItems = new List<CharacterItem>();
-
-            if (CacheLootBagRandomItems.Count > 0)
-            {
-                ItemDrop randomItem;
-                for (int countDrops = 0; countDrops < CacheLootBagRandomItems.Count && countDrops < maxRandomLootItems; ++countDrops)
-                {
-                    randomItem = CacheLootBagRandomItems[Random.Range(0, CacheLootBagRandomItems.Count)];
-                    if (Random.value > randomItem.dropRate)
-                        continue;
-                    LootItems.Add(CharacterItem.Create(randomItem.item.DataId, 1, (short)Random.Range(randomItem.minAmount <= 0 ? 1 : randomItem.minAmount, randomItem.maxAmount)));
-                }
-                if (LootItems.Count == 0)
-                {
-                    randomItem = CacheLootBagRandomItems[Random.Range(0, CacheLootBagRandomItems.Count)];
-                    LootItems.Add(CharacterItem.Create(randomItem.item.DataId, 1, (short)Random.Range(randomItem.minAmount <= 0 ? 1 : randomItem.minAmount, randomItem.maxAmount)));
-                }
-            }
-
-            return LootItems;
-        }
-
-        /// <summary>
-        /// Returns a list of random item drops.
-        /// </summary>
-        public List<ItemDrop> CacheLootBagRandomItems
+        private List<ItemDrop> cacheRandomLootItems = null;
+        public List<ItemDrop> CacheRandomLootItems
         {
             get
             {
-                if (cacheRandomLootBagItems == null)
+                if (cacheRandomLootItems == null)
                 {
                     int i;
-                    cacheRandomLootBagItems = new List<ItemDrop>();
+                    cacheRandomLootItems = new List<ItemDrop>();
                     if (randomLootBagItems != null &&
                         randomLootBagItems.Length > 0)
                     {
@@ -77,16 +46,16 @@ namespace MultiplayerARPG
                                 randomLootBagItems[i].maxAmount <= 0 ||
                                 randomLootBagItems[i].dropRate <= 0)
                                 continue;
-                            cacheRandomLootBagItems.Add(randomLootBagItems[i]);
+                            cacheRandomLootItems.Add(randomLootBagItems[i]);
                         }
                     }
-
-                    if (lootBagItemDropTables != null && lootBagItemDropTables.Length > 0)
+                    if (lootBagItemDropTables != null &&
+                        lootBagItemDropTables.Length > 0)
                     {
                         foreach (ItemDropTable itemDropTable in lootBagItemDropTables)
                         {
-                            if (itemDropTable != null && 
-                                itemDropTable.randomItems != null && 
+                            if (itemDropTable != null &&
+                                itemDropTable.randomItems != null &&
                                 itemDropTable.randomItems.Length > 0)
                             {
                                 for (i = 0; i < itemDropTable.randomItems.Length; ++i)
@@ -95,24 +64,66 @@ namespace MultiplayerARPG
                                         itemDropTable.randomItems[i].maxAmount <= 0 ||
                                         itemDropTable.randomItems[i].dropRate <= 0)
                                         continue;
-                                    cacheRandomLootBagItems.Add(itemDropTable.randomItems[i]);
+                                    cacheRandomLootItems.Add(itemDropTable.randomItems[i]);
                                 }
                             }
                         }
                     }
-                    cacheRandomLootBagItems.Sort((a, b) => b.dropRate.CompareTo(a.dropRate));
-                    certainDropItems.Clear();
-                    uncertainDropItems.Clear();
-                    for (i = 0; i < cacheRandomLootBagItems.Count; ++i)
+                    cacheRandomLootItems.Sort((a, b) => b.dropRate.CompareTo(a.dropRate));
+                    certainLootItems.Clear();
+                    uncertainLootItems.Clear();
+                    for (i = 0; i < cacheRandomLootItems.Count; ++i)
                     {
-                        if (cacheRandomLootBagItems[i].dropRate >= 1f)
-                            certainDropItems.Add(cacheRandomLootBagItems[i]);
+                        if (cacheRandomLootItems[i].dropRate >= 1f)
+                            certainLootItems.Add(cacheRandomLootItems[i]);
                         else
-                            uncertainDropItems.Add(cacheRandomLootBagItems[i]);
+                            uncertainLootItems.Add(cacheRandomLootItems[i]);
                     }
                 }
-                return cacheRandomLootBagItems;
+                return cacheRandomLootItems;
             }
+        }
+
+        /// <summary>
+        /// Returns a random collection of item drops.
+        /// </summary>
+        /// <returns>list of random item drops</returns>
+        public List<CharacterItem> GetRandomItems()
+        {
+            List<CharacterItem> items = new List<CharacterItem>();
+
+            if (CacheRandomLootItems.Count == 0)
+                return items;
+
+            int randomDropCount = 0;
+            int i;
+
+            // Add certain loot rate items
+            certainLootItems.Shuffle();
+            for (i = 0; i < certainLootItems.Count && randomDropCount < maxLootItems; ++i)
+            {
+                short amount = (short)Random.Range(certainLootItems[i].minAmount <= 0 ? 1 : certainLootItems[i].minAmount, certainLootItems[i].maxAmount);
+                items.Add(CharacterItem.Create(certainLootItems[i].item, 1, amount));
+                randomDropCount++;
+            }
+
+            // Reached max loot items?
+            if (randomDropCount >= maxLootItems)
+                return items;
+            
+            // Add uncertain loot rate items
+            uncertainLootItems.Shuffle();
+            for (i = 0; i < uncertainLootItems.Count && randomDropCount < maxLootItems; ++i)
+            {
+                if (Random.value >= uncertainLootItems[i].dropRate)
+                    continue;
+
+                short amount = (short)Random.Range(uncertainLootItems[i].minAmount <= 0 ? 1 : uncertainLootItems[i].minAmount, uncertainLootItems[i].maxAmount);
+                items.Add(CharacterItem.Create(uncertainLootItems[i].item, 1, amount));
+                randomDropCount++;
+            }
+
+            return items;
         }
     }
 }
